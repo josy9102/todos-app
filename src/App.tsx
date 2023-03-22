237f566f31efe3267d6b2dcd9456c2b9ac791c33
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-03-22 14:40:05 +0100

    aded new to do component and started todo add handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
-  
+
+  const todoAddHandler = (todoText: string) => {
+    console.log(todoText);
+  };
+
   return (
     <div className="App">
-      {/* component that adds todos */}
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos} />
     </div>
   );
